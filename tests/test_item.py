0d89ee14529e1b299629f442389f5b9f39cca29b
belvvika <bellvika132@gmail.com>
2023-07-01 11:49:54 +0300

    add test item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+def test_init_item(get_Item):
+    assert get_Item
+
+def test_calculate_total_price(get_Item):
+    assert get_Item.calculate_total_price() == 500
+
+def test_apply_discount(get_Item):
+    item = get_Item
+    item.pay_rate = 0.8
+    assert item.apply_discount() == 80
+
+
+
+
