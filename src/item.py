cc3d9cf1292a0083e0c3e28c4910c528ed740a5d
belvvika <bellvika132@gmail.com>
2023-08-16 10:30:52 +0300

    update item.py

--- src/item.py
+++ src/item.py
@@ -1,4 +1,4 @@
-
+import csv
 class Item:
     """
     Класс для представления товара в магазине.
@@ -14,10 +14,11 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         '''
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
@@ -32,3 +33,32 @@ class Item:
         """
         self.price = self.price * self.pay_rate
         return self.price
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        '''
+        Инициализирует экземпляры класса Item данными из файла src/items.csv
+        '''
+        with open('../src/items.csv') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=",")
+            for row in reader:
+                item = cls(row['name'], row['price'], row['quantity'])
+
+    @staticmethod
+    def string_to_number(number):
+        '''
+        Возвращает число из числа-строки
+        '''
+        numbers = number.split('.')
+        return int(number[0])
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name_new):
+        if len(name_new) > 10:
+            self.__name = name_new[:10]
+        else:
+            self.__name = name_new
+
