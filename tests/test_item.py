8128f0a390ca579f385b26088a59caf6320654b7
belvvika <bellvika132@gmail.com>
2023-08-16 10:31:27 +0300

    update test item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,5 +1,4 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-
 def test_init_item(get_Item):
     assert get_Item
 
@@ -11,6 +10,8 @@ def test_apply_discount(get_Item):
     item.pay_rate = 0.8
     assert item.apply_discount() == 80
 
-
-
+def test_instantiate_from_csv():
+   pass
+def test_string_to_number():
+    pass
 
