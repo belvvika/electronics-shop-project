01703c83d1aaa0169671d0cafb818df9ac6ae7a5
belvvika <134885414+belvvika@users.noreply.github.com>
2023-10-29 12:46:46 +0300

    update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
 import pytest
 from src.item import Item
+from src.phone import Phone
 
 @pytest.fixture
 def get_Item():
     return Item('hello', 100, 5 )
+
+@pytest.fixture
+def get_Phone():
+    return Item('hello', 100, 5, 4)
