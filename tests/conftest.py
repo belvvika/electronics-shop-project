8b075bd63ee77bf3e5957a3ed0134d700850fc66
belvvika <134885414+belvvika@users.noreply.github.com>
2023-11-01 23:47:00 +0300

    update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from src.item import Item
 from src.phone import Phone
+from src.keyboard import KeyBoard, Key_Lang
 
 @pytest.fixture
 def get_item():
@@ -9,3 +10,7 @@ def get_item():
 @pytest.fixture
 def get_phone():
     return Phone('hello', 100, 5, 4)
+
+@pytest.fixture
+def get_keyboard():
+    return KeyBoard('hello', 100, 5, 'EN')
